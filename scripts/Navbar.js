0868d12552e4fcc6c091a9b408ecb83bda5fc958
Miquéias Franco <121680934+MiqueiasFranco@users.noreply.github.com>
2023-09-14 17:32:05 -0300

    criação de elementos do carrinho

--- scripts/Navbar.js
+++ scripts/Navbar.js
@@ -4,18 +4,39 @@ var content = document.getElementById('content')
 var container = document.getElementById('container')
 var showSidebar = false;
 
-function toogleSidebar(){
+var Carrinhoid = document.getElementById('carrinho');
+var carrinho = false;
+
+
+function toogleSidebar() {
     showSidebar = !showSidebar;
-    if(showSidebar){
-        navlist.style.marginLeft='-10vw';
-        navlist.style.animationName='showSidebar';
-        content.style.filter='blur(2px)'
-        container.style.filter='blur(2px)'
+    if (showSidebar) {
+        navlist.style.marginLeft = '-10vw';
+        navlist.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)'
+        container.style.filter = 'blur(2px)'
     }
-    else{
-        navlist.style.marginLeft='-100vw';
-        navlist.style.animationName='noSee';
-        content.style.filter=''
-        container.style.filter=''
+    else {
+        navlist.style.marginLeft = '-100vw';
+        navlist.style.animationName = 'noSee';
+        content.style.filter = ''
+        container.style.filter = ''
     }
+}
+
+function carrinhoOnOf() {
+    carrinho = !carrinho;
+    if (carrinho) {
+        Carrinhoid.style.marginRight = '0vw';
+        Carrinhoid.style.animationName = 'CarrinhoOn';
+        content.style.filter = 'blur(2px)'
+        container.style.filter = 'blur(2px)'
+    }
+    else {
+        Carrinhoid.style.marginRight = '-20vw';
+        Carrinhoid.style.animationName = 'CarrinhoOf';
+        content.style.filter = ''
+        container.style.filter = ''
+    }
+
 }
--- scripts/produtos.js
+++ scripts/produtos.js
@@ -5,47 +5,71 @@ const produtos = [
         nome: 'Iphone XR',
         marca: 'Apple',
         preco: 3000,
+        imagem: 'https://exitocol.vtexassets.com/arquivos/ids/4815447-800-auto?width=800&height=auto&aspect=true',
     },
     {
         id: 2,
         nome: 'Iphone 11 plus',
         marca: 'Apple',
-        preco: 3000,
+        preco: 3500,
+        imagem: 'https://www.thekase.com/on/demandware.static/-/Sites-tk-product/default/dwf266d1c2/38960428/132183_medium.png',
     },
     {
         id: 3,
         nome: 'Iphone 12',
         marca: 'Apple',
-        preco: 3000,
+        preco: 4000,
+        imagem: 'https://ipartner.com.ua/image/cache/catalog/imgmainru2_191/clearapple-iphone-11-128-gb-red-krasnij-680x630.jpeg',
     },
     {
         id: 4,
         nome: 'Iphone 12 plus',
         marca: 'Apple',
-        preco: 3000,
+        preco: 4500,
+        imagem: 'https://firstmarkt.es/wp-content/uploads/2021/10/iphone-13-pro-max-grafito-3.png',
 
     },
     {
         id: 5,
         nome: 'Iphone 13',
         marca: 'Apple',
-        preco: 3000,
+        preco: 5000,
+        imagem: 'https://th.bing.com/th/id/OIP.q9DCMoF6e-pG-y0Hq_9mMQHaHa?pid=ImgDet&rs=1',
     },
 ];
+
 
 for (const produto of produtos) {
     const cardproduto = `<figure class="item" id="card-${produto.id}">
     <p>${produto.nome}</p>
-    <p>${produto.marca}</p>
-    <p>R$ ${produto.preco}</p>
-    <a class="button">Comprar</a>
+    <p >${produto.marca}</p>
+    <p id="preco">R$ ${produto.preco}</p>
+    <a id="adicionar${produto.id}" class="button">Adicionar</a>
     </figure>`;
     document.getElementById('container').innerHTML += cardproduto;
+
+}
+for (const produto of produtos) {
+    document.getElementById(`adicionar${produto.id}`).addEventListener("click", () => adicionarAoCarrinho(produto.id));
 }
 
 
-
-
+function adicionarAoCarrinho(idProduto) {
+    const produto = produtos.find((p) => p.id === idProduto);
+    const produtoCarrinho = `
+    <article class="produto" id="produto${produto.id}">
+        <img src="${produto.imagem}"
+        alt="${produto.nome}">
+      <div>
+        <p>${produto.nome}</p>
+        <p id="cor">${produto.marca}</p>
+        <p id="preco">R$ ${produto.preco}</p>
+        <button id="comprar">Comprar</button>
+      </div>
+    </article>`;
+    document.getElementById('carrinho').innerHTML += produtoCarrinho;
+    carrinhoOnOf();
+}
 
 
 
