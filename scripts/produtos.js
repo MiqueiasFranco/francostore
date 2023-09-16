b19de8fea32a1325473f71ab04649b079692a1d3
Miquéias Franco <121680934+MiqueiasFranco@users.noreply.github.com>
2023-09-16 12:17:55 -0300

    adição de banner no body e funções ao carrinho

--- scripts/produtos.js
+++ scripts/produtos.js
@@ -1,28 +1,28 @@
 
 const produtos = [
     {
-        id: 1,
+        id: '1',
         nome: 'Iphone XR',
         marca: 'Apple',
         preco: 3000,
         imagem: 'https://exitocol.vtexassets.com/arquivos/ids/4815447-800-auto?width=800&height=auto&aspect=true',
     },
     {
-        id: 2,
+        id: '2',
         nome: 'Iphone 11 plus',
         marca: 'Apple',
         preco: 3500,
         imagem: 'https://www.thekase.com/on/demandware.static/-/Sites-tk-product/default/dwf266d1c2/38960428/132183_medium.png',
     },
     {
-        id: 3,
+        id: '3',
         nome: 'Iphone 12',
         marca: 'Apple',
         preco: 4000,
         imagem: 'https://ipartner.com.ua/image/cache/catalog/imgmainru2_191/clearapple-iphone-11-128-gb-red-krasnij-680x630.jpeg',
     },
     {
-        id: 4,
+        id: '4',
         nome: 'Iphone 12 plus',
         marca: 'Apple',
         preco: 4500,
@@ -30,7 +30,7 @@ const produtos = [
 
     },
     {
-        id: 5,
+        id: '5',
         nome: 'Iphone 13',
         marca: 'Apple',
         preco: 5000,
@@ -43,31 +43,68 @@ for (const produto of produtos) {
     const cardproduto = `<figure class="item" id="card-${produto.id}">
     <p>${produto.nome}</p>
     <p >${produto.marca}</p>
-    <p id="preco">R$ ${produto.preco}</p>
+    <p>R$ ${produto.preco}</p>
     <a id="adicionar${produto.id}" class="button">Adicionar</a>
     </figure>`;
     document.getElementById('container').innerHTML += cardproduto;
 
 }
+
 for (const produto of produtos) {
     document.getElementById(`adicionar${produto.id}`).addEventListener("click", () => adicionarAoCarrinho(produto.id));
 }
 
+function atualizarquantidade(idProduto) {
+    document.getElementById(`quantidade${idProduto}`).innerText = idquantidadeproduto[idProduto];
+}
+
+function incrementarproduto(idProduto) {
+    if (idProduto in idquantidadeproduto) {
+        idquantidadeproduto[idProduto]++
+        atualizarquantidade(idProduto)
+    }
+}
+function decrementarproduto(idProduto) {
+    if (idProduto in idquantidadeproduto) {
+        idquantidadeproduto[idProduto]--;
+        atualizarquantidade(idProduto);
+    }
+}
+
+const idquantidadeproduto = {};
+
+
 
 function adicionarAoCarrinho(idProduto) {
+    if (idProduto in idquantidadeproduto) {
+        incrementarproduto(idProduto)
+        return
+    }
+    idquantidadeproduto[idProduto] = 1
+
+    const elementoArticle = document.createElement('article');
+    elementoArticle.classList.add('produto');
+    const carrinhoid = document.getElementById('carrinho');
     const produto = produtos.find((p) => p.id === idProduto);
     const produtoCarrinho = `
-    <article class="produto" id="produto${produto.id}">
         <img src="${produto.imagem}"
-        alt="${produto.nome}">
-      <div>
-        <p>${produto.nome}</p>
-        <p id="cor">${produto.marca}</p>
-        <p id="preco">R$ ${produto.preco}</p>
-        <button id="comprar">Comprar</button>
-      </div>
-    </article>`;
-    document.getElementById('carrinho').innerHTML += produtoCarrinho;
+            alt="${produto.nome}">
+        <div>
+            <p>${produto.nome}</p>
+            <p id="cor">${produto.marca}</p>
+            <p id="preco">R$ ${produto.preco}</p>
+            <button id="comprar">Comprar</button>
+        </div>
+        <div class="botoes-carrinho">
+            <button id="decrementar${produto.id}">-</button>
+            <p id="quantidade${produto.id}">${idquantidadeproduto[produto.id]}</p>
+            <button id="incrementar${produto.id}">+</button>
+        </div>`;
+    elementoArticle.innerHTML = produtoCarrinho
+    carrinhoid.appendChild(elementoArticle);
+    document.getElementById(`decrementar${produto.id}`).addEventListener('click', () => decrementarproduto(produto.id));
+    document.getElementById(`incrementar${produto.id}`).addEventListener('click', () => incrementarproduto(produto.id));
+
     carrinhoOnOf();
 }
 
